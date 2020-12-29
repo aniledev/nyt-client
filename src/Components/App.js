5752f323b3f5fc3d3dec0e4a66768ba721a1b623
aniledev <elinamcgill@gmail.com>
2020-12-29 03:06:51 -0500

    chore: add comment psuedocode

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,5 +1,6 @@
 import "../Styles/App.css";
 
+// this function will contain a form to capture user input
 function App() {
   return <div className="App"></div>;
 }
---
+++ src/Components/Book.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+// this component will display the details of a book
+export default class Book extends React.Component {
+  render() {
+    return <div></div>;
+  }
+}
