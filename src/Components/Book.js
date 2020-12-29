26768d777589e26c6cf92e7e5052781095972efb
aniledev <elinamcgill@gmail.com>
2020-12-29 03:08:10 -0500

    feat: create Book component JSX elements

--- src/Components/Book.js
+++ src/Components/Book.js
@@ -1,8 +1,18 @@
 import React from "react";
 
-// this component will display the details of a book
-export default class Book extends React.Component {
-  render() {
-    return <div></div>;
-  }
+// this component will display the details of a book so this can be a plain function component
+
+export default function Book(props) {
+  return (
+    <div className="Book">
+      <h2>{props.title}</h2>
+      <div className="book_author">by {props.author}</div>
+      <div className="book_publisher">
+        Published by: {props.publisher}
+        on {moment(props.published_date).format("DD MMM YYYY")}
+      </div>
+      <div className="book_description">{props.description}</div>
+      <div className="book_details">Rank {props.rank} this week</div>
+    </div>
+  );
 }
